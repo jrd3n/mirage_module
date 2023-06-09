dc3f02ca022dab6b5807f071caa38651cec1821f
Jordon Archer <jordon_services@hotmail.co.uk>
2023-06-09 14:45:30 +0100

    added glasses

--- ble_GLASSES.py
+++ ble_GLASSES.py
@@ -2,7 +2,7 @@ from mirage.core import module
 from mirage.libs import utils,ble,io
 from mirage.core import module,interpreter
 
-class ble_BULB(module.WirelessModule):
+class ble_GLASSES(module.WirelessModule):
     def init(self):
 
         self.technology = "ble"
@@ -10,7 +10,7 @@ class ble_BULB(module.WirelessModule):
         self.description = "Connection module for Bluetooth Low Energy devices"
         self.args = {
                 "INTERFACE":"hci0",
-                "TARGET":"BE:58:97:00:1C:62",
+                "TARGET":"00:3A:FE:00:CA:87",
                 "TIMEOUT":"10",
                 "CONNECTION_TYPE":"public"
             }
@@ -41,47 +41,47 @@ class ble_BULB(module.WirelessModule):
             if self.receiver.isConnected():
                 io.success("Connected on device : "+self.args["TARGET"])
 
-                io.chart(["Input","Action"],[["r","red"],["g","green"],["b","blue"],["w","white"],["party","party lights"],["on","on"],["off","off"]])
+                io.chart(["Input","Action"],[["cross","cross"],["heart","heart"]])
 
                 while True:
 					
                     key = io.ask("User input")
 
-                    if key == "r":
-                        self.send_value(0x9,'7e070503ff000010ef')
+                    if key == "hearts":
+                        self.send_value(0x12,'fd00dedf98a17e5c2c7d2268eb6d042b')
                         # value = int('7e0783073a0405ffef', 16)
                         # self.emitter.send(ble.BLEWriteCommand(handle=0x9,value=value))
                     # if the key is 'down arrow' ...
-                    elif key == "g":
+                    elif key == "cross":
                         # inject a OFF packet
-                        self.send_value(0x9,'7e07050300ff0010ef')
-                    elif key == "b":
-                        # inject a OFF packet
-                        self.send_value(0x9,'7e0705030000ff10ef')
-                    elif key == "s":
-                        # inject a OFF packet
-                        self.send_value(0x9,'7e04016401ffff00ef')
-                    elif key == "w":
-                        # inject a OFF packet
-                        self.send_value(0x9,'7e070503ffffff10ef')
-                    elif key == "on":
-                        self.send_value(0x9,'7e04016401ffff00ef')
-                    elif key == "off":
-                        self.send_value(0x9,'7e04010001ffff00ef')
-                    elif key == "party":
+                        self.send_value(0x12,'8788fc7cf5f64d386a94eb980c281b12')
+                    # elif key == "b":
+                    #     # inject a OFF packet
+                    #     self.send_value(0x9,'7e0705030000ff10ef')
+                    # elif key == "s":
+                    #     # inject a OFF packet
+                    #     self.send_value(0x9,'7e04016401ffff00ef')
+                    # elif key == "w":
+                    #     # inject a OFF packet
+                    #     self.send_value(0x9,'7e070503ffffff10ef')
+                    # elif key == "on":
+                    #     self.send_value(0x9,'7e04016401ffff00ef')
+                    # elif key == "off":
+                    #     self.send_value(0x9,'7e04010001ffff00ef')
+                    # elif key == "party":
                                                         
-                        for i in range(10):
+                    #     for i in range(10):
 
-                            self.send_value(0x9,'7e07050300ff0010ef')
-                            utils.wait(seconds=0.2)
+                    #         self.send_value(0x9,'7e07050300ff0010ef')
+                    #         utils.wait(seconds=0.2)
 
-                            self.send_value(0x9,'7e0705030000ff10ef')
-                            utils.wait(seconds=0.2)
+                    #         self.send_value(0x9,'7e0705030000ff10ef')
+                    #         utils.wait(seconds=0.2)
 
-                            self.send_value(0x9,'7e070503ff000010ef')
-                            utils.wait(seconds=0.2)
+                    #         self.send_value(0x9,'7e070503ff000010ef')
+                    #         utils.wait(seconds=0.2)
 
-                            io.progress(i,10)
+                    #         io.progress(i,10)
 
                     elif key == 'q':
                         exit()
--- move_to.sh
+++ move_to.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo cp ./ble_BULB.py /root/.mirage/modules/ble_BULB.py
-sudo cp ./ble_bulb_scene.py /root/.mirage/scenarios/ble_bulb_scene.py
 
 sudo cp ./ble_JORDON.py /root/.mirage/modules/ble_JORDON.py
-sudo cp ./ble_trial_scene.py /root/.mirage/scenarios/ble_trial_scene.py
+
+sudo cp ./ble_GLASSES.py /root/.mirage/modules/ble_GLASSES.py
 
 echo "Moved!"
 
